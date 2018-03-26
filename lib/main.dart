c576ffb834e6855273d38d19e4c8c22d0bc5b2a7
Olufemi Salaam <salaam.olufemi@gmail.com>
2018-03-25 20:50:42 -0400

    Radia Home Page Shell

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,22 @@
 import 'package:flutter/material.dart';
-
+import 'package:english_words/english_words.dart';
 import './pages/landing_page.dart';
 
-void main(){
-  runApp(new MaterialApp(
-    home: new LandingPage(),
-  ));
+void main() => runApp(new MyApp());
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return new MaterialApp(
+      title: 'Radia',
+      home: new Scaffold(
+        appBar: new AppBar(
+          title: new Text('Radia'),
+        ),
+        body: new Center(
+          child: new Text('Welcome to Radia'),
+        ),
+      ),
+    );
+  }
 }
