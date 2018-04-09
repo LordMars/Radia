4e6485b3e65e4d7e66e785ef071aec0ec76dfbfa
Jason Ogato <jason.ogato@bison.howard.edu>
2018-04-09 19:54:16 -0400

    Formatted page according to recommended dart style

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,28 +1,31 @@
-import 'package:flutter/material.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'dart:async';
-import 'dart:convert';
+import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
 import './main_page.dart';
-import '../utils/user.dart';
-import '../ui/loading_overlay.dart';
 import '../ui/code_input.dart';
+import '../ui/loading_overlay.dart';
 import '../ui/phone_input.dart';
+import '../utils/user.dart';
 
-class LoginPage extends StatefulWidget{
-  
+class LoginPage extends StatefulWidget {
+  final String loginConfirmationURL =
+      "https://us-central1-radia-personal-build.cloudfunctions.net/loginConfirmationCode";
+
+  final String loginVerificationURL =
+      "https://us-central1-radia-personal-build.cloudfunctions.net/loginVerifyUserCode";
+
   @override
   State createState() => new LoginPageState();
 }
 
-class LoginPageState extends State<LoginPage>{
-
+class LoginPageState extends State<LoginPage> {
   String countryCode, phoneNumber;
   bool overlay, verify;
-  
+
   final scaffoldKey = new GlobalKey<ScaffoldState>();
 
-  Future<String> findSavedPhone() async{
+  Future<String> findSavedPhone() async {
     String uid = '';
     SharedPreferences preferences = await SharedPreferences.getInstance();
     uid = preferences.getString('uid');
@@ -33,148 +36,149 @@ class LoginPageState extends State<LoginPage>{
   * cloud function. Receives a success or
   * error as a return
   */
-  Future<bool> sendLoginCode()async{
-    var uri = Uri.parse('https://us-central1-radia-personal-build.cloudfunctions.net/loginConfirmationCode');
+  Future<bool> sendLoginCode() async {
+    var uri = Uri.parse(widget.loginConfirmationURL);
     String result;
     bool success = false;
 
-    try{
-      await http.post(uri, body: {"phone": User.getInstance().phone, "uid": User.getInstance().uid})
-	  .then((response){
-        if(response.statusCode == 200){
+    try {
+      await http.post(uri, body: {
+        "phone": User.getInstance().phone,
+        "uid": User.getInstance().uid
+      }).then((response) {
+        if (response.statusCode == 200) {
           result = 'Sending Confirmation Text';
           success = true;
-        }
-        else{
+        } else {
           result = 'Error sending phone number';
           success = false;
         }
-        scaffoldKey.currentState.showSnackBar(  //Alert user text has been sent
-        new SnackBar(
-            content: new Text(result),
-          )
-        );
-
+        scaffoldKey.currentState.showSnackBar(//Alert user text has been sent
+            new SnackBar(
+          content: new Text(result),
+        ));
       });
-    }catch(exception){
-        scaffoldKey.currentState.showSnackBar(  //Alert user an error occured sending text
-        new SnackBar(
-          content: const Text('Error Sending Phone Number'),
-        )
-      );
+    } catch (exception) {
+      scaffoldKey.currentState
+          .showSnackBar(//Alert user an error occured sending text
+              new SnackBar(
+        content: const Text('Error Sending Phone Number'),
+      ));
       success = false;
     }
     return success;
   }
 
-  Future<bool> verifyLoginCode()async{
-    var uri = Uri.parse('https://us-central1-radia-personal-build.cloudfunctions.net/loginVerifyUserCode');
+  Future<bool> verifyLoginCode() async {
+    var uri = Uri.parse(widget.loginVerificationURL);
     String result;
     bool success = false;
 
-    try{
+    try {
       await http.post(uri, body: {
-        "radiaCode":User.getInstance().code,
+        "radiaCode": User.getInstance().code,
         "uid": User.getInstance().uid,
         "phoneNumber": User.getInstance().phone
-        }).then((response){
-        
-        if(response.statusCode == 200){
+      }).then((response) {
+        if (response.statusCode == 200) {
           result = 'Code Confirmed!';
           success = true;
-        }
-        else{
+        } else {
           result = 'Invalid Confirmation Code';
           success = false;
         }
-        scaffoldKey.currentState.showSnackBar(  //Alert user text has been sent
-        new SnackBar(
-            content: new Text(result),
-          )
-        );
-
+        scaffoldKey.currentState.showSnackBar(//Alert user text has been sent
+            new SnackBar(
+          content: new Text(result),
+        ));
       });
-    }catch(exception){
-        scaffoldKey.currentState.showSnackBar(  //Alert user an error occured sending text
-        new SnackBar(
-          content: const Text('Error Sending Confirmation Code'),
-        )
-      );
+    } catch (exception) {
+      scaffoldKey.currentState
+          .showSnackBar(//Alert user an error occured sending text
+              new SnackBar(
+        content: const Text('Error Sending Confirmation Code'),
+      ));
       success = false;
     }
     return success;
   }
 
-  void showOverlay(){
-    setState((){
+  void showOverlay() {
+    setState(() {
       overlay = true;
     });
 
-    if(!verify){ 
-     sendLoginCode().then((success){
-       hideOverlay();
-       if(success){
-         verify = true;
-       }
-     })
-     .catchError((onError) => verify = false);
-    }
-    else{
-      verifyLoginCode().then((success) async{
+    if (!verify) {
+      sendLoginCode().then((success) {
         hideOverlay();
-        if(success){
+        if (success) {
+          verify = true;
+        }
+      }).catchError((onError) => verify = false);
+    } else {
+      verifyLoginCode().then((success) async {
+        hideOverlay();
+        if (success) {
           Navigator.of(context).pushAndRemoveUntil(
-            new MaterialPageRoute(builder: (context) => new HomePage()), 
-            (Route route) => route == null);
+              new MaterialPageRoute(builder: (context) => new HomePage()),
+              (Route route) => route == null);
         }
       });
     }
   }
 
   //Hides the overlay
-  void hideOverlay(){
-    setState((){
+  void hideOverlay() {
+    setState(() {
       overlay = false;
     });
-
   }
 
-
   @override
-  void initState(){
+  void initState() {
     super.initState();
     countryCode = "+1";
     phoneNumber = "";
     overlay = false;
     verify = false;
-    findSavedPhone().then((uid){
-      if (uid != null && uid != ''){
+    findSavedPhone().then((uid) {
+      if (uid != null && uid != '') {
         User.getInstance().setUid = uid;
       }
     });
   }
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return new WillPopScope(
       onWillPop: () async => false,
       child: new Scaffold(
         key: scaffoldKey,
         body: new Stack(
           children: <Widget>[
-            new Column(
-              children: <Widget>[(overlay) ? new Container() : new Padding(
-                padding: const EdgeInsets.only(top: 60.0),
-                child: new Container(
-                  child: const Text("Radia", style: const TextStyle(
-                    color: Colors.blue, 
-                    fontWeight: FontWeight.bold, 
-                    fontSize: 30.0),
+            new Column(children: <Widget>[
+              (overlay)
+                  ? new Container()
+                  : new Padding(
+                      padding: const EdgeInsets.only(top: 60.0),
+                      child: new Container(
+                        child: const Text(
+                          "Radia",
+                          style: const TextStyle(
+                              color: Colors.blue,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 30.0),
+                        ),
+                      ),
                     ),
-                ),
+              new Padding(
+                padding: new EdgeInsets.only(bottom: 20.0),
               ),
-              new Padding(padding: new EdgeInsets.only(bottom: 20.0),),
-            (overlay) ? new Container() : ((verify) ? new CodeInput(showOverlay) : new PhoneInput(showOverlay, null, 0)),
+              (overlay)
+                  ? new Container()
+                  : ((verify)
+                      ? new CodeInput(showOverlay)
+                      : new PhoneInput(showOverlay, null, 0)),
             ]),
             (overlay) ? new LoadingOverlay() : new Container()
           ],
